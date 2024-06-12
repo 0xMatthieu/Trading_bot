d841226cdaee0ce040406e70002b9b6f3f48ede9
Matt <Matt1607@hotmail.com>
2024-06-12 18:58:02 +0200

    bux fix in Heikin Ashi /2

--- Trading_tools.py
+++ Trading_tools.py
@@ -51,13 +51,13 @@ def calculate_heikin_ashi(df):
     ha_df = df.copy()
     ha_df['HA_Close'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
     
-    ha_open = [df['Open'][0]]
+    ha_open = [df['open'][0]]
     for i in range(1, len(df)):
         ha_open.append((ha_open[-1] + ha_df['HA_Close'][i-1]) / 2)
     ha_df['HA_Open'] = ha_open
     
-    ha_df['HA_High'] = ha_df[['HA_Open', 'HA_Close', 'High']].max(axis=1)
-    ha_df['HA_Low'] = ha_df[['HA_Open', 'HA_Close', 'Low']].min(axis=1)
+    ha_df['HA_High'] = ha_df[['HA_Open', 'HA_Close', 'high']].max(axis=1)
+    ha_df['HA_Low'] = ha_df[['HA_Open', 'HA_Close', 'low']].min(axis=1)
     
     return ha_df
 	
