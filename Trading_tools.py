5e1a9935ab8cbb22b338a1a5e299bf96868baf1b
Matthieu <111059326+0xMatthieu@users.noreply.github.com>
2024-06-12 14:24:25 +0200

    Update Trading_tools.py / macd heikin

--- Trading_tools.py
+++ Trading_tools.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pandas_ta as ta
 import numpy as np
 
-def calculate_macd(df, fast=12, slow=26, signal=9):
+def calculate_macd(df, fast=12, slow=26, signal=9, column='close'):
 	"""
 	Calculate MACD and determine buy/sell signals.
 
@@ -24,7 +24,7 @@ def calculate_macd(df, fast=12, slow=26, signal=9):
 		return df, None, None
 
 	# Calculate MACD
-	macd = ta.macd(df['close'], fast=fast, slow=slow, signal=signal)
+	macd = ta.macd(df[column], fast=fast, slow=slow, signal=signal)
     
 	macd_column_name = f'MACD'			#f'MACD_{fast}_{slow}_{signal}'
 	signal_column_name = f'MACDs'		#f'MACDs_{fast}_{slow}_{signal}'
@@ -46,8 +46,22 @@ def calculate_macd(df, fast=12, slow=26, signal=9):
 	#trend = 'buy' if df[macd_column_name].iloc[0] > df[signal_column_name].iloc[0] else 'sell'
 
 	return df
-
+	
 def calculate_heikin_ashi(df):
+    ha_df = df.copy()
+    ha_df['HA_Close'] = (df['Open'] + df['High'] + df['Low'] + df['Close']) / 4
+    
+    ha_open = [df['Open'][0]]
+    for i in range(1, len(df)):
+        ha_open.append((ha_open[-1] + ha_df['HA_Close'][i-1]) / 2)
+    ha_df['HA_Open'] = ha_open
+    
+    ha_df['HA_High'] = ha_df[['HA_Open', 'HA_Close', 'High']].max(axis=1)
+    ha_df['HA_Low'] = ha_df[['HA_Open', 'HA_Close', 'Low']].min(axis=1)
+    
+    return ha_df
+	
+def heikin_ashi_strategy(df):
 	ha_df = df.copy()
 	ha_df['HA_Close'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
     
